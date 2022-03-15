2971d208e838879b57112cb4dfc4b20348b03b25
Oskar Ferens <oskar.ferens1@gmail.com>
2022-03-15 18:47:14 +0100

    sth

---
+++ FileLineCounter/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+//        Stwórz program który zliczy liczbę linii tekstu dla wszystkich plików w wskazanym folderze
+//        Wynikiem powinna być lista plików wraz z informacją o liczbie linii oraz suma wszystkich linii w folderze.
+//        Generalnie wypakuj i wrzuć do classpath
--- StreamTask/src/main/java/Main.java
+++ StreamTask/src/main/java/Main.java
@@ -1,33 +1,39 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 class Main {
-    public static void main(String[] args) {
-        List<Person> list = new ArrayList<>() {{
-            add(new Person("Kasia", "Kaczmarek", 15));
-            add(new Person("Zosia", "Zosiewicz", 25));
-            add(new Person("Bartek", "Dziadosz", 35));
-            add(new Person("Ukasz", "Lecz", 45));
-            add(new Person("Bartek", "Dziadczyk", 21));
-            add(new Person("Kacper", "Kaczmarek", 43));
-            add(new Person("Mateusz", "Mateuszczykl", 43));
-        }};
+    List<Person> list = new ArrayList<>() {{
+        add(new Person("Kasia", "Kaczmarek", 15));
+        add(new Person("Zosia", "Zosiewicz", 25));
+        add(new Person("Bartek", "Dziadosz", 35));
+        add(new Person("Ukasz", "Lecz", 45));
+        add(new Person("Bartek", "Dziadczyk", 21));
+        add(new Person("Kacper", "Kaczmarek", 43));
+        add(new Person("Mateusz", "Mateuszczykl", 43));
+    }};
 
+    public static void findShortSurnames (final List<Person> list) {
+        final List<String> collect = list.stream()
+                .map()
+                .filter()
+                .forEach();
+    }
 
-        list.stream()
-                .map(a -> a.getSurname())
-                .forEach(System.out::println);
+    public static void main(final String[] args) {
+        final List <Person> people = List.of(new Person("Arek","Acki",22));
+        findShortSurnames(people);
+    }
+}
 
-        /*
-        1) Zwroc liste osób które mają wiecej niż 5 liter w naziwsku.
+/*
+        1) Zwroc liste osób które mają wiecej niż 5 liter w nazwisku.
         2) Zwroc srednia wieku z podanych osób.
         3) Zwroc srednia wieku z osob ktore maja mniej niż 30 lat.
         4) Znajdz pierwsza osobe z listy której nazwisko zaczyna się na D.
         5) Sprawdz czy jest z osoba w liscie z imieniem Bartek i Krzysztof.
         6) Zsumuj wiek osób któe mają mniej niż 26 lat.
         */
-    }
-}
 
 //        IntStream.range(100,200)
 //                .filter(i -> i%3 ==0 && i%5 ==0)
