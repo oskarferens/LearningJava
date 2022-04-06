b35f01077a49ce919aff208973890c1becaa2258
Oskar Ferens <oskar.ferens1@gmail.com>
2022-04-06 20:45:07 +0200

    FileCounterEx done

--- FileLineCounter/src/main/java/Main.java
+++ FileLineCounter/src/main/java/Main.java
@@ -8,13 +8,14 @@ import static java.nio.file.Files.lines;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println("Podaj sciezke do folderu");
+        System.out.println("Podaj nazwe pliku");
 
         Scanner scanner = new Scanner(System.in);
-        Path path = Path.of("C:/Users/oskar/IdeaProjects/JavaClass/FileLineCounter/Files/TextFiles/example.txt");
-        System.out.println(path.getFileName());
+        String path = scanner.nextLine();
+        Path pathToFile = Path.of("C:/Users/oskar/IdeaProjects/JavaClass/FileLineCounter/Files/TextFiles/" + path);
+        System.out.println(pathToFile.getFileName());
 
-        try (Stream <String> stream = lines(path)){
+        try (Stream <String> stream = lines(pathToFile)){
             System.out.println(stream.count());
         } catch (IOException e) {
             e.printStackTrace();
@@ -28,15 +29,11 @@ public class Main {
 //        Generalnie wypakuj i wrzuć do classpath
 
 //        Procesowanie plikow
-//        przekazywanie przez referencje i przez wartosc, czym sie rozni.
 //        instancja klasy
-//        wlaczyc procesowanie adnotacji - znalezc ock
 //        program sie zaczyna i wyswietla sie komunikat "podaj sciezke do folderu, ktory ma byc przeskanowany"
 //        ten plik musze patha skopiowac.
 //        ZROBIC sama metode ktora procesuje linijki, ktora na sztywno sprawdzi jakas sciezke string path = nazwa ciezki
-//        i z tego patha zrobic plik i sprawdzac  czy ten plik istnieje lub przejsc od razu do czytania linijek
-//
-//        sprawdzic czy folder XDDD cos zawiera
+//        i z tego patha zrobic plik i sprawdzac  czy ten plik istnieje lub przejsc od razu do czytania linije
 //
 //        STAGE 2 dodac petle
 //        ktora czeka az wprowadze nazwe pliku i patrzylbym qwteey czy ten plik jestw  pathie
